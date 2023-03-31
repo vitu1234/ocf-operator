dd0e5b046a09cdd88cffce04f1a1f75cb1c2c066
vitu1234 <mafeniblessed@gmail.com>
2023-04-01 00:45:16 +0900

    added automation CRD

--- main.go
+++ main.go
@@ -110,6 +110,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "OCFDeviceResource")
 		os.Exit(1)
 	}
+	if err = (&controllers.AutomationReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Automation")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
